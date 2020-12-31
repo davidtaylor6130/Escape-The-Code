05711d92702f2933a047c39e00c7a7a4e774f337
davidtaylor6130 <davidtaylor613@gmail.com>
2020-12-31 21:36:31 +0000

    Started Properly Working on the game

---
+++ GradsInGames-sfas_2021_unity-424d8c29d8c2/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float PlayerMovementSpeed = 1.0f;
+    public GameObject Player;
+    public Rigidbody CC;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical")); // <---Get Input From Unity Input System
+        movement = transform.TransformDirection(movement * PlayerMovementSpeed);
+        CC.velocity = movement;
+    }
+}
