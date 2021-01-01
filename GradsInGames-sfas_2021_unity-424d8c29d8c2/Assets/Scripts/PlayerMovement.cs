7c447c7c48bf3c11b63617e9747c52331da7b861
davidtaylor6130 <davidtaylor613@gmail.com>
2021-01-01 17:19:40 +0000

    Jumping Improvements (grounded check and stopping in air jumps)

--- GradsInGames-sfas_2021_unity-424d8c29d8c2/Assets/Scripts/PlayerMovement.cs
+++ GradsInGames-sfas_2021_unity-424d8c29d8c2/Assets/Scripts/PlayerMovement.cs
@@ -11,12 +11,13 @@ public class PlayerMovement : MonoBehaviour
 
     private Vector3 rotation;
     private Rigidbody rb;
-
+    private float CenterToGround;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        CenterToGround = GetComponent<SphereCollider>().bounds.extents.y;
     }
 
     // Update is called once per frame
@@ -30,7 +31,7 @@ public class PlayerMovement : MonoBehaviour
     void Move()
     {
         //- Movement Code -//
-        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical")); // <---Get Input From Unity Input System
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
         movement = transform.TransformDirection(movement * PlayerMovementSpeed);
         movement.y = rb.velocity.y;
         rb.velocity = movement;
@@ -38,11 +39,13 @@ public class PlayerMovement : MonoBehaviour
 
     void Jump()
     {
-        if (Input.GetButtonDown("Jump"))
+        //- jump check -//
+        if (Input.GetButtonDown("Jump") && isGrounded(0.1f))
         {
             GetComponent<Rigidbody>().velocity = (Vector3.up * 5);
         }
 
+        //- slow decent when holding jump button and faster decent when space is just tapped -//
         if (rb.velocity.y < 0)
         {
             rb.velocity += Vector3.up * Physics.gravity.y * (2.5f - 1) * Time.deltaTime;
@@ -55,8 +58,15 @@ public class PlayerMovement : MonoBehaviour
 
     void Look()
     {
-        //Vector3 rotationInput = new Vector3(((Input.GetAxis("Mouse Y") * rotationSpeed) * Time.deltaTime), ((Input.GetAxis("Mouse X") * rotationSpeed) * Time.deltaTime), 0.0f);
-        //rotation += rotationInput;
-        //TargetPosition.eulerAngles = rotation;
+        Vector3 rotationInput = new Vector3(0.0f , ((Input.GetAxis("Mouse X") * rotationSpeed) * Time.deltaTime), 0.0f);
+        rotation += rotationInput;
+        TargetPosition.eulerAngles = rotation;
+    }
+
+    //- Query Functions -//
+    bool isGrounded(float af_leeway)
+    {
+       //- fire raycast down and uses the distance to fround for length plus leeway to allow check to work on all terrain -//
+        return Physics.Raycast(transform.position, -Vector3.up, CenterToGround + af_leeway);
     }
 }
