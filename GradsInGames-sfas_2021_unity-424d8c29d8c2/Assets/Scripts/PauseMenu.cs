9c60dbe8baa553ed5b882b190be9bd7a0ef054ba
davidtaylor6130 <davidtaylor613@gmail.com>
2021-01-23 20:40:35 +0000

    Added Pause Menu

---
+++ GradsInGames-sfas_2021_unity-424d8c29d8c2/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject PauseMenuObject;
+    public GameObject[] ObjectsToToggleOff;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Time.timeScale = 0;
+            PauseMenuObject.SetActive(true);
+        }
+    }
+
+    public void LeaveMenu()
+    {
+        Time.timeScale = 1;
+        PauseMenuObject.SetActive(false);
+        Debug.Log("Left Pause Menu");
+    }
+
+    public void ReturnToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+        Debug.Log("returned TO Main Menu");
+    }
+}
