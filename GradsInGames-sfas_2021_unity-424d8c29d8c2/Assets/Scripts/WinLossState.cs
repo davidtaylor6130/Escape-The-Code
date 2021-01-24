811e9f78b4b9ca6807541082d3697d143c987ee7
davidtaylor6130 <davidtaylor613@gmail.com>
2021-01-24 01:44:02 +0000

    Created Win loss state controller

---
+++ GradsInGames-sfas_2021_unity-424d8c29d8c2/Assets/Scripts/WinLossState.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[System.Serializable]
+public enum WinLossStates
+{
+    Good = 0,
+    Neurtal,
+    Bad,
+    BatteryDeath,
+    FoundDeath
+};
+
+public class WinLossState : MonoBehaviour
+{
+    public GameObject ParentGui;
+
+    public GameObject[] WinlossCondition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ParentGui.SetActive(false);
+    }
+
+    public void SetWinLossCondition(WinLossStates state)
+    {
+        //- turn off all possible win loss texts -//
+        foreach (GameObject StateOption in WinlossCondition)
+        {
+            StateOption.SetActive(false);
+        }
+
+        //- turn on correct one -//
+        WinlossCondition[(int)state].SetActive(true);
+        
+        //- turn on entire gui window -//
+        ParentGui.SetActive(true);
+    }
+
+    public void ReturnToMainMenu()
+    {
+        Debug.Log("returned TO Main Menu");
+        SceneManager.LoadScene("MainMenuScene");
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("It Quit");
+        Application.Quit();
+    }
+}
